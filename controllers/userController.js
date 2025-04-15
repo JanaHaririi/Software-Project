eea966aa58b44338ef961d9a7297e6a55b64b040
Habiba <habibaw006@gmail.com>
2025-04-15 19:52:46 +0300

    set jwt as cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,7 +50,11 @@ const loginUser = async (req, res) => {
       const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
         expiresIn: '7d'
       });
-  
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      });
       // 4. Respond
       res.json({
         id: user._id,
