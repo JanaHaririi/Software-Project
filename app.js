ff9e984a23a25e179f4c1429443ab1c94551c77a
shahdyehiaa <shahd.shoaib@student.giu-uni.de>
2025-05-25 23:47:47 +0300

    .

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app._router.stack.forEach(r => {
