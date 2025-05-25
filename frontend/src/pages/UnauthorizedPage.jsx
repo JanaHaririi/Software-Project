86fb504ab592feb9fe62bf709b634d2530aad31a
shahdyehiaa <shahd.shoaib@student.giu-uni.de>
2025-05-25 23:38:21 +0300

    ,

--- frontend/src/pages/UnauthorizedPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function UnauthorizedPage() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h2> Access Denied</h2>
-      <p>You do not have permission to view this page.</p>
-    </div>
-  );
-}
--- frontend/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-// utils/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
-  withCredentials: true,
-});
-
-export default api;
